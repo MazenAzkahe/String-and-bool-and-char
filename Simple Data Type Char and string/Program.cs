f4a3dd36fbe5e00a0c4f583115bb07a4c211f281
Mazen AZKAHE <ENGMAZENN93@Outlook.com>
2018-04-02 21:51:33 -0700

    string and bool and char

---
+++ Simple Data Type Char and string/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Data_Type_Char_and_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chMin = Char.MinValue;
+            int chMax = char.MaxValue;
+            int MyChar = 'S';
+            char MyCharAsCh = 'S';
+          
+            Console.WriteLine(Convert.ToChar(83));
+            Console.WriteLine(Convert.ToChar(81));
+            Console.WriteLine("chMin={0} , chMax={1} , Mychar={2},Char As int={3}" , chMin , chMax , MyChar,MyCharAsCh);
+            string myString = "Hello ";
+            Console.WriteLine(myString);
+
+            bool Mybool = true;
+            bool Mybool2 = false;
+            Console.WriteLine(Mybool);
+            Console.WriteLine(Mybool2);
+                             
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
